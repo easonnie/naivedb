6c7b42986df08f49220d3b3b55391f5e338cc504
Nengbao Liu <nengbao@cs.uchicago.edu>
2016-03-18 23:35:58 -0500

    update BufferPool

--- src/java/simpledb/BufferPool.java
+++ src/java/simpledb/BufferPool.java
@@ -18,8 +18,8 @@ public class BufferPool {
     public static final int PAGE_SIZE = 4096;
 
     /** Default number of pages passed to the constructor. This is used by
-    other classes. BufferPool should use the numPages argument to the
-    constructor instead. */
+     other classes. BufferPool should use the numPages argument to the
+     constructor instead. */
     public static final int DEFAULT_PAGES = 50;
     int UpperBoundNum;  /** The actual upperbound of page number  */
     Map<PageId, Page> pageMap;
@@ -56,10 +56,11 @@ public class BufferPool {
      * @param perm the requested permissions on the page
      */
     public  Page getPage(TransactionId tid, PageId pid, Permissions perm)
-        throws TransactionAbortedException, DbException {
+            throws TransactionAbortedException, DbException {
         // some code goes here
 
         //Lock
+//         @TODO transacitons waiting for locks are not queued?
         boolean isLock = lockManager.LockOn(perm, tid, pid);
         long start = System.currentTimeMillis();
         while (!isLock) {
@@ -133,7 +134,7 @@ public class BufferPool {
      * @param commit a flag indicating whether we should commit or abort
      */
     public void transactionComplete(TransactionId tid, boolean commit)
-        throws IOException {
+            throws IOException {
         // some code goes here
         // not necessary for proj1
         if (commit) {
@@ -156,20 +157,20 @@ public class BufferPool {
 
     /**
      * Add a tuple to the specified table behalf of transaction tid.  Will
-     * acquire a write lock on the page the tuple is added to(Lock 
-     * acquisition is not needed for lab2). May block if the lock cannot 
+     * acquire a write lock on the page the tuple is added to(Lock
+     * acquisition is not needed for lab2). May block if the lock cannot
      * be acquired.
-     * 
+     *
      * Marks any pages that were dirtied by the operation as dirty by calling
-     * their markDirty bit, and updates cached versions of any pages that have 
-     * been dirtied so that future requests see up-to-date pages. 
+     * their markDirty bit, and updates cached versions of any pages that have
+     * been dirtied so that future requests see up-to-date pages.
      *
      * @param tid the transaction adding the tuple
      * @param tableId the table to add the tuple to
      * @param t the tuple to add
      */
     public void insertTuple(TransactionId tid, int tableId, Tuple t)
-        throws DbException, IOException, TransactionAbortedException {
+            throws DbException, IOException, TransactionAbortedException {
         // some code goes here
         // not necessary for proj1
         DbFile dbFile = Database.getCatalog().getDbFile(tableId);
@@ -186,7 +187,7 @@ public class BufferPool {
      * the lock cannot be acquired.
      *
      * Marks any pages that were dirtied by the operation as dirty by calling
-     * their markDirty bit.  Does not need to update cached versions of any pages that have 
+     * their markDirty bit.  Does not need to update cached versions of any pages that have
      * been dirtied, as it is not possible that a new page was created during the deletion
      * (note difference from addTuple).
      *
@@ -194,7 +195,7 @@ public class BufferPool {
      * @param t the tuple to add
      */
     public  void deleteTuple(TransactionId tid, Tuple t)
-        throws DbException, TransactionAbortedException {
+            throws DbException, TransactionAbortedException {
         // some code goes here
         // not necessary for proj1
         int tableId = t.getRecordId().getPageId().getTableId();
@@ -219,13 +220,13 @@ public class BufferPool {
     }
 
     /** Remove the specific page id from the buffer pool.
-        Needed by the recovery manager to ensure that the
-        buffer pool doesn't keep a rolled back page in its
-        cache.
-    */
+     Needed by the recovery manager to ensure that the
+     buffer pool doesn't keep a rolled back page in its
+     cache.
+     */
     public synchronized void discardPage(PageId pid) {
         // some code goes here
-	// not necessary for proj1
+        // not necessary for proj1
 
     }
 
@@ -251,7 +252,7 @@ public class BufferPool {
         // some code goes here
         // not necessary for proj1
         for (Page page : pageMap.values()) {
-            if (tid.equals(page.isDirty())) {
+            if (page.isDirty() != null && tid.equals(page.isDirty())) {
                 flushPage(page.getId());
             }
         }
@@ -267,15 +268,20 @@ public class BufferPool {
         Long oldestTime = Long.MAX_VALUE;
         PageId LRUPageId = null;
         for (PageId pageId : lastUsedTimeMap.keySet()) {
+            Page page = pageMap.get(pageId);
             Long time = lastUsedTimeMap.get(pageId);
-            if (time < oldestTime) {
+            if ((page.isDirty() == null) && (time < oldestTime)) {
                 oldestTime = time;
                 LRUPageId = pageId;
             }
         }
 
-        pageMap.remove(LRUPageId);
-        lastUsedTimeMap.remove(LRUPageId);
+        if (LRUPageId == null) {
+            throw new DbException("Exception in BufferPool, all pages in the bufferpoll are dirty, fail to evict page to disk.");
+        } else {
+            pageMap.remove(LRUPageId);
+            lastUsedTimeMap.remove(LRUPageId);
+        }
     }
 
 //    class HeapNode implements Comparable<HeapNode> {
